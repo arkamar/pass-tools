91d977fb2ed84fe4e47f2ecb5199ecd5da57b052
Petr Vaněk <arkamar@atlas.cz>
2017-04-24 01:21:00 +0200

    Search optionally user name

--- passget.c
+++ passget.c
@@ -11,23 +11,25 @@ char * argv0;
 static
 void
 usage() {
-	fprintf(stderr, "usage: %s service\n", argv0);
+	fprintf(stderr, "usage: %s service [username]\n", argv0);
 	exit(1);
 }
 
 int
 main(int argc, char * argv[]) {
-	char * patt, * line = NULL;
+	char * patt, * user = NULL, * line = NULL;
 	size_t cap = 0;
 	ssize_t len, found = 0;
 	struct pass pass;
 
 	argv0 = *argv; argv++; argc--;
 
-	if (argc != 1)
+	if (argc != 1 && argc != 2)
 		usage();
 
 	patt = *argv; argv++; argc--;
+	if (argv && *argv)
+		user = *argv; argv++; argc--;
 
 	if (isatty(fileno(stdout))) {
 		fprintf(stderr, "%s does not print pass phrases to ttys, "
@@ -38,7 +40,8 @@ main(int argc, char * argv[]) {
 	}
 
 	while ((len = getdelim(&line, &cap, RECORD_SEPARATOR, stdin)) > 0) {
-		if (pass_parse(&pass, line) && strstr(pass.service, patt)) {
+		if (pass_parse(&pass, line) && strstr(pass.service, patt)
+				&& (!user ? 1 : !!strstr(pass.username, user))) {
 			found++;
 			pass_print(&pass);
 			if (found == 1)
@@ -50,9 +53,15 @@ main(int argc, char * argv[]) {
 	free(line);
 
 	if (found == 0)
-		fprintf(stderr, "No record found for pattern '%s'\n", patt);
+		fprintf(stderr, "No record found for service pattern '%s'", patt);
 	if (found > 1)
-		fprintf(stderr, "Multiple records (%lu) found for pattern '%s'\n", found, patt);
+		fprintf(stderr, "Multiple records (%lu) found for service pattern '%s'", found, patt);
+	if (found == 0 || found > 1) {
+		if (user)
+			fprintf(stderr, " and user pattern '%s'\n", user);
+		else
+			fputc('\n', stderr);
+	}
 
 	return found != 1;
 }
