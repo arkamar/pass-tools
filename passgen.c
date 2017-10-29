75b7f355280162600d687b6dae65e29c853bb224
Petr Vaněk <arkamar@atlas.cz>
2017-10-30 00:19:58 +0100

    [passgen] password type

--- passgen.c
+++ passgen.c
@@ -1,9 +1,25 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#include "arg.h"
 #include "pass.h"
+
+enum pass_tr {
+	PASS_TR_PRINT,
+	PASS_TR_ALNUM,
+	PASS_TR_ALNUM_HAS_UNDER,
+};
+
+char * argv0;
+
+void
+usage() {
+	fprintf(stderr, "usage: %s [-ab]\n", argv0);
+	exit(PASS_ERROR);
+}
 
 int
 pass_getrandom(void * in, size_t size) {
@@ -49,12 +65,19 @@ tr_alphanum_hyp_uder(unsigned char c) {
 		return '_';
 }
 
+unsigned char (*callback[])(unsigned char) = {
+	[PASS_TR_PRINT] = tr_all_printable,
+	[PASS_TR_ALNUM] = tr_alphanum,
+	[PASS_TR_ALNUM_HAS_UNDER] = tr_alphanum_hyp_uder
+};
+
 int
-pass_tr(unsigned char * pass, const size_t len) {
+pass_tr(unsigned char * pass, const size_t len,
+		unsigned char (*tr_fun)(unsigned char)) {
 	int i;
-	for (i = 0; i < len; i++) {
-		pass[i] = tr_alphanum_hyp_uder(pass[i]);
-	}
+	for (i = 0; i < len; i++)
+		pass[i] = tr_fun(pass[i]);
+
 	return PASS_SUCCESS;
 }
 
@@ -63,10 +86,23 @@ main(int argc, char * argv[]) {
 	unsigned char buf[256];
 	size_t len = sizeof buf;
 	int i;
+	enum pass_tr pt = PASS_TR_PRINT;
+
+	ARGBEGIN {
+	case 'a':
+		pt = PASS_TR_ALNUM;
+		break;
+	case 'b':
+		pt = PASS_TR_ALNUM_HAS_UNDER;
+		break;
+	default:
+		usage();
+	} ARGEND
+
 	for (i = 0; i < len; i++)
 		buf[i] = i;
-//	len = pass_getrandom(buf, sizeof buf);
-	pass_tr(buf, len);
+	len = pass_getrandom(buf, sizeof buf);
+	pass_tr(buf, len, callback[pt]);
 	fwrite(buf, 1, len, stdout);
 	return PASS_SUCCESS;
 }
