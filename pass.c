51f68e4e141aa52a4cb8fe5741aba96e4c624ba1
Petr Vaněk <arkamar@atlas.cz>
2017-11-05 18:23:33 +0100

    [pass] use xmemset

--- pass.c
+++ pass.c
@@ -81,7 +81,7 @@ pass_parse(struct pass * restrict pass, char * restrict line) {
 	const char ** arr = (void *)pass;
 	char * s;
 	int i;
-	memset(pass, 0, sizeof *pass);
+	xmemset(pass, 0, sizeof *pass);
 	for (s = line, i = 0; *s; s++) {
 		if (i < (sizeof * pass / sizeof(char *)) && !arr[i])
 			arr[i] = s;
